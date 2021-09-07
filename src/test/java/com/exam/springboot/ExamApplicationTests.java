2bbe4c61c43ff00ed0aad25bcf5fe19836985c67
Thien Phuoc <anhvodoi0989666874@gmail.com>
2021-09-07 15:07:06 +0700

    login spring security

---
+++ src/test/java/com/exam/springboot/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
